cda0bd321101af9c8ff60322bbcceb6b7d832458
jinsso2 <wlstjqkr1234@gmail.com>
2021-06-20 18:19:13 +0900

    Final

---
+++ EventForm/EventForm/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EventForm
+{
+    public partial class Form1 : Form
+    {
+        private int elapsedTime = 0;
+
+        class CustomForm : Form
+        {
+            public CustomForm()
+            {
+                Text = "제목 글자";
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += Button1_Click;
+            FormClosed += Form1_FormClosed;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+         //   throw new NotImplementedException();
+        }
+
+        
+    private void Button1_Click(object sender, EventArgs e)
+        {
+            Button self = (Button)sender;
+            self.Text = "저를 클릭했습니다";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "+";
+            label1.Text += "+";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            elapsedTime++;
+            label2.Text = elapsedTime + "초 경과";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // MessageBox.Show("내용");
+            // MessageBox.Show("내용", "제목");
+            DialogResult result;
+            do
+            {
+                result = MessageBox.Show("내용", "제목", MessageBoxButtons.RetryCancel);
+            } while (result == DialogResult.Retry);
+            // => "다시하기" 버튼을 누르는 동안에는 창이 계속해서 떠있도록
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            CustomForm form = new CustomForm();
+            form.ShowDialog();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            CustomForm form = new CustomForm();
+            form.Show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Form2 form = new Form2();
+            form.Show();
+        }
+    }
+}
---
+++ EventForm/EventForm/Form2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EventForm
+{
+    public partial class Form2 : Form
+    {
+        class CustomForm1 : Form
+        {
+            public CustomForm1()
+            {
+                Text = "제목 글자";
+            }
+        }
+        public Form2()
+        {
+            InitializeComponent();
+            IsMdiContainer = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CustomForm1 cstForm = new CustomForm1();
+            cstForm.MdiParent = this;
+            cstForm.Show();
+        }
+    }
+}
